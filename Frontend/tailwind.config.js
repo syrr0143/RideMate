d921248135da2f62124926799432bc2638dba0f4
syrr0143 <sy816120@gmail.com>
2024-12-31 15:00:17 +0530

    added vercel.json

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,8 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")],
+// };
+// tailwind.config.js
+
+import daisyui from "daisyui"; // Import daisyui
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui], // Use the imported daisyui
 };
