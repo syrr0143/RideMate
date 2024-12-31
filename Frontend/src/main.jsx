678e9629780d1061949cc87f553d7c89f947a964
syrr0143 <sy816120@gmail.com>
2024-12-31 16:25:38 +0530

    updated

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { SocketProvider } from "./context/SocketIoContext.jsx";
 import { MapContextProvider } from "./hooks/useHidden.jsx";
@@ -12,9 +12,9 @@ createRoot(document.getElementById("root")).render(
     <SocketProvider>
       <AuthProvider>
         <MapContextProvider>
-          <BrowserRouter>
+          <HashRouter>
             <App />
-          </BrowserRouter>
+          </HashRouter>
         </MapContextProvider>
       </AuthProvider>
     </SocketProvider>
